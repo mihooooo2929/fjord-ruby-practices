735fa46ecf756b55aab7b381e75e51df0dde2382
Miho Uchida <44660466+miho2929@users.noreply.github.com>
2021-07-12 15:32:00 +0900

    引数に月と年を指定した場合は任意のカレンダーを表示できるようにした

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,24 +1,45 @@
 require 'date'
 
-year = Date.today.year.to_s
-month = Date.today.month.to_s
-this_month_date = Date.new(Time.now.year, Time.now.month, -1).day
-week = ["日", "月", "火", "水", "木", "金", "土"]
-header = month + "月" + " " + year 
-first_day_weekday = Date.new(Time.now.year, Time.now.month, 1).wday
+if ARGV[0] && ARGV[1]
+    year = ARGV[1].to_i
+    month = ARGV[0].to_i
+    this_month_date = Date.new(year, month, -1).day
+    week = ["日", "月", "火", "水", "木", "金", "土"]
+    header = month.to_s + "月" + " " + year.to_s
+    first_day_weekday = Date.new(year, month, 1).wday
 
-puts first_day_weekday
-puts header.center(20) 
-puts week.join(" ") 
-print "   " * first_day_weekday
+    puts header.center(20) 
+    puts week.join(" ") 
+    print "   " * first_day_weekday
 
-i = 1
-while i <= this_month_date
-    print i.to_s.rjust(2) + " "
-    i += 1
-    if (i + first_day_weekday -1) % 7 == 0
-        print "\n"
+    i = 1
+    while i <= this_month_date
+        print i.to_s.rjust(2) + " "
+        i += 1
+        if (i + first_day_weekday -1) % 7 == 0
+            print "\n"
+        end
     end
-end
+    print "\n"
+else
+    year = Time.now.year
+    month = Time.now.month
+    this_month_date = Date.new(year, month, -1).day
+    week = ["日", "月", "火", "水", "木", "金", "土"]
+    header = month.to_s + "月" + " " + year.to_s 
+    first_day_weekday = Date.new(year, month, 1).wday
 
-print "\n"
+    puts header.center(20) 
+    puts week.join(" ") 
+    print "   " * first_day_weekday
+
+    i = 1
+    while i <= this_month_date
+        print i.to_s.rjust(2) + " "
+        i += 1
+        if (i + first_day_weekday -1) % 7 == 0
+            print "\n"
+        end
+    end
+    print "\n"
+end
