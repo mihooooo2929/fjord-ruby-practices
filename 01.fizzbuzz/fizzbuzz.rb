ddf04233a4c0c8f1e407f4f23d5684cfceaeb29b
Miho Uchida <44660466+miho2929@users.noreply.github.com>
2021-07-16 20:16:50 +0900

    インデントの深さを調整

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,13 +1,13 @@
 n = 1
 while n < 21 do
-    if n % 15 == 0
-        puts "fizzbuzz"  
-    elsif n % 3 == 0
-        puts "fizz"
-    elsif n % 5 == 0
-        puts "buzz"
-    else 
-        puts n
-    end
-    n += 1
+  if n % 15 == 0
+    puts "fizzbuzz"  
+  elsif n % 3 == 0
+    puts "fizz"
+  elsif n % 5 == 0
+    puts "buzz"
+  else 
+    puts n
+  end
+  n += 1
 end
