083decfd7b2b5d904b907ce2c7d49061f446c036
Miho Uchida <44660466+miho2929@users.noreply.github.com>
2021-07-17 18:40:23 +0900

    余計なコードを削除し、rubocopを走らせてリファクタリングをした

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 score = ARGV[0]
 scores = score.split(',')
 shots = []
 scores.each do |s|
-  if s == 'X'
+  case s
+  when 'X'
     shots << 10
     shots << 0
-  elsif s == 'S' #spare
-    shots << 10
   else
     shots << s.to_i
   end
@@ -21,20 +22,16 @@ point = 0
 frames.each_with_index do |frame, i|
   if frame[0] == 10 # strike
     if i < 10
-      if frames[i + 1][0] != 10
-        point += frames[i + 1].sum + 10
-      else
-        point += frames[i + 1][0] + frames[i + 2][0] + 10
-      end
+      point += if frames[i + 1][0] != 10
+                 frames[i + 1].sum + 10
+               else
+                 frames[i + 1][0] + frames[i + 2][0] + 10
+               end
     end
   elsif frame.sum == 10 # spare
-    if i < 10
-      point += frames[i + 1][0] + 10
-    end
-  else
-    if i < 10
-      point += frame.sum
-    end
+    point += frames[i + 1][0] + 10 if i < 10
+  elsif i < 10
+    point += frame.sum
   end
 end
 puts point
