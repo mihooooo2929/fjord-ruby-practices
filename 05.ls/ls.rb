045bd0af76509d3bee5eb267307af5306d4b1fb3
Miho Uchida <44660466+miho2929@users.noreply.github.com>
2021-07-21 00:37:10 +0900

    rubocopを通すための修正

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -9,29 +9,27 @@ puts "\n"
 set_max_column = 3
 
 items = if options['a'] == true && options['l'] == true && options['r'] == true
-          Dir::entries('.').sort.reverse
+          Dir.entries('.').reverse
         elsif options['a'] == true && options['l'] == true && options['r'] == false
-          Dir::entries('.').sort
+          Dir.entries('.').sort
         elsif options['a'] == true && options['r'] == true && options['l'] == false
-          Dir::entries('.').sort.reverse
+          Dir.entries('.').reverse
         elsif options['l'] == true && options['r'] == true && options['a'] == false
-          Dir.glob('*').sort.reverse
+          Dir.glob('*').reverse
         elsif options['a'] == true && options['l'] == false && options['r'] == false
-          Dir::entries('.').sort
+          Dir.entries('.').sort
         elsif options['l'] == true && options['a'] == false && options['r'] == false
           Dir.glob('*').sort
         elsif options['r'] == true && options['a'] == false && options['l'] == false
-          Dir.glob('*').sort.reverse
+          Dir.glob('*').reverse
         else
-          items = Dir.glob('*')
+          Dir.glob('*')
         end
 
-window_length = (`tput cols`).to_i 
-max_item_string_length = 0 
+window_length = `tput cols`.to_i
+max_item_string_length = 0
 items.each do |item|
-  if max_item_string_length <= item.to_s.length
-    max_item_string_length = item.to_s.length
-  end
+  max_item_string_length = item.to_s.length if max_item_string_length <= item.to_s.length
 end
 number_of_columns = window_length / (max_item_string_length + 1)
 number_of_columns = set_max_column if number_of_columns > set_max_column
@@ -42,42 +40,40 @@ while i != 0 && options['l'] != true
   i = items.length % number_of_columns
 end
 
-def method_for_general(items,number_of_columns,max_item_string_length)
-    number_of_rows = (items.length / number_of_columns.to_i).to_i
-    transposed_items = items.each_slice(number_of_rows).to_a.transpose
-    number_of_t_items = transposed_items.length
-    this_item_x = 0
-    ary = []
-    while this_item_x < transposed_items.length do
-      this_item_y = 0
-      while this_item_y < number_of_columns do
-        print transposed_items[this_item_x][this_item_y].ljust(max_item_string_length.to_i + 1,' ')
-        ary << transposed_items[this_item_x][this_item_y]
-        this_item_y += 1
-      end
-      this_item_x += 1
-      ary.each_with_index do |a, i|
-        if i % (number_of_columns * this_item_x) == 0
-        print "\n"
-        end
-      end
+def method_for_general(items, number_of_columns, max_item_string_length)
+  number_of_rows = (items.length / number_of_columns.to_i).to_i
+  transposed_items = items.each_slice(number_of_rows).to_a.transpose
+  this_item_x = 0
+  ary = []
+  while this_item_x < transposed_items.length
+    this_item_y = 0
+    while this_item_y < number_of_columns
+      print transposed_items[this_item_x][this_item_y].ljust(max_item_string_length.to_i + 1, ' ')
+      ary << transposed_items[this_item_x][this_item_y]
+      this_item_y += 1
     end
+    this_item_x += 1
+    ary.each_with_index do |_, i|
+      print "\n" if (i % (number_of_columns * this_item_x)).zero?
+    end
+  end
 end
 
 def method_for_l(items)
   items.each do |item|
     file_path = File.absolute_path(item.to_s)
     file_stat = File.stat(file_path)
-    file_mode_2octal = file_stat.mode.to_s(2).rjust(8,'0')
-    file_mode_2octal_permission = (file_mode_2octal.to_i / 1) % 1000000000
+    file_mode_2octal = file_stat.mode.to_s(2).rjust(8, '0')
+    file_mode_2octal_permission = (file_mode_2octal.to_i / 1) % 1_000_000_000
     permission_number_ary = file_mode_2octal_permission.to_s.split('')
     file_type = file_stat.ftype
-    if file_type == 'directory'
-        file_type = 'd'
-    elsif file_type == 'file'
-        file_type = '-'
-    elsif file_type == 'link'
-        file_type - 'l'
+    case file_type
+    when 'directory'
+      file_type = 'd'
+    when 'file'
+      file_type = '-'
+    when 'link'
+      file_type - 'l'
     end
     file_size = file_stat.size
     file_time_mtime = file_stat.mtime
@@ -85,44 +81,44 @@ def method_for_l(items)
     file_time_mtime_d = file_time_mtime.day
     file_time_mtime_hour = file_time_mtime.hour
     file_time_mtime_min = file_time_mtime.min
-    file_link = file_stat.nlink 
+    file_link = file_stat.nlink
     permission_number_ary.each_with_index do |n, i|
-        if n == '1' && i == 0
-        n = 'r'
-        elsif n == '1' && i == 3
-        n = 'r'
-        elsif n == '1' && i == 6
-        n = 'r'
-        elsif n == '1' && i == 1
-        n = 'w'
-        elsif n == '1' && i == 4
-        n = 'w'
-        elsif n == '1' && i == 7
-        n = 'w'
-        elsif n == '1' && i == 2
-        n = 'x'
-        elsif n == '1' && i == 5
-        n = 'x'
-        elsif n == '1' && i == 8
-        n = 'x'
-        else
-        n = '-' 
-        end
-        permission_number_ary[i] = n
+      n = if n == '1' && i.zero?
+            'r'
+          elsif n == '1' && i == 3
+            'r'
+          elsif n == '1' && i == 6
+            'r'
+          elsif n == '1' && i == 1
+            'w'
+          elsif n == '1' && i == 4
+            'w'
+          elsif n == '1' && i == 7
+            'w'
+          elsif n == '1' && i == 2
+            'x'
+          elsif n == '1' && i == 5
+            'x'
+          elsif n == '1' && i == 8
+            'x'
+          else
+            '-'
+          end
+      permission_number_ary[i] = n
     end
     permission = permission_number_ary.join('')
     owner_uid = file_stat.uid
     file_owner = Etc.getpwuid(owner_uid).name
     group_uid = file_stat.gid
     file_group = Etc.getgrgid(group_uid).name
-    print file_type + permission + ' ' + file_link.to_s.rjust(2,' ') + ' ' + file_owner + ' ' + file_group  + ' ' + file_size.to_s.rjust(7,' ') + ' ' + file_time_mtime_m.to_s.rjust(2,' ') + ' ' + file_time_mtime_d.to_s + ' ' + file_time_mtime_hour.to_s.rjust(2,'0') + ':' + file_time_mtime_min.to_s.rjust(2,'0') + ' ' + item.to_s
+    print "#{file_type}#{permission} #{file_link.to_s.rjust(2, ' ')} #{file_owner} #{file_group} #{file_size.to_s.rjust(7, ' ')} #{file_time_mtime_m.to_s.rjust(2, ' ')} #{file_time_mtime_d} #{file_time_mtime_hour.to_s.rjust(2, '0')} #{file_time_mtime_min.to_s.rjust(2, '0')} #{item}"
     print "\n"
   end
 end
 
 if options['l'] == true
-    method_for_l(items)
-else options['l'] != true
-    method_for_general(items,number_of_columns,max_item_string_length)
+  method_for_l(items)
+else
+  method_for_general(items, number_of_columns, max_item_string_length)
 end
 print "\n"
