4f8a15e0d9887fab6bc6687cabc01f7bac798cd2
Miho Uchida <44660466+miho2929@users.noreply.github.com>
2021-07-20 21:41:43 +0900

    長いコードをメソッドにして分けた

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -5,7 +5,6 @@ require 'etc'
 
 options = ARGV.getopts('alr')
 puts "\n"
-puts options
 
 set_max_column = 3
 
@@ -27,7 +26,6 @@ items = if options['a'] == true && options['l'] == true && options['r'] == true
           items = Dir.glob('*')
         end
 
-
 window_length = (`tput cols`).to_i 
 max_item_string_length = 0 
 items.each do |item|
@@ -38,45 +36,35 @@ end
 number_of_columns = window_length / (max_item_string_length + 1)
 number_of_columns = set_max_column if number_of_columns > set_max_column
 
-
 i = items.length % number_of_columns
 while i != 0 && options['l'] != true
   items.push('')
   i = items.length % number_of_columns
 end
 
-if options['l'] != true
-  number_of_rows = (items.length / number_of_columns.to_i).to_i
-  transposed_items = items.each_slice(number_of_rows).to_a.transpose
-  puts i.to_s + ':itemの配列要素数/カラム数'
-  puts items.length.to_s + ':itemsの配列の要素数' 
-  puts max_item_string_length.to_s + ':一番長い名前の文字数'
-  puts window_length.to_s + ':ウィンドウの幅の文字数'
-  puts number_of_rows.to_s + ':行の数'
-  puts transposed_items.length.to_s + ':transpose後の配列の数' 
-  puts number_of_columns.to_s + ':カラムの数'
-  puts "\n"
-  number_of_t_items = transposed_items.length
-  this_item_x = 0
-  ary = []
-  while this_item_x < transposed_items.length do
-    this_item_y = 0
-    while this_item_y < number_of_columns do
-      print transposed_items[this_item_x][this_item_y].ljust(max_item_string_length.to_i + 1,' ')
-      ary << transposed_items[this_item_x][this_item_y]
-      this_item_y += 1
-    end
-    this_item_x += 1
-    ary.each_with_index do |a, i|
-      if i % (number_of_columns * this_item_x) == 0
-      print "\n"
+def method_for_general(items,number_of_columns,max_item_string_length)
+    number_of_rows = (items.length / number_of_columns.to_i).to_i
+    transposed_items = items.each_slice(number_of_rows).to_a.transpose
+    number_of_t_items = transposed_items.length
+    this_item_x = 0
+    ary = []
+    while this_item_x < transposed_items.length do
+      this_item_y = 0
+      while this_item_y < number_of_columns do
+        print transposed_items[this_item_x][this_item_y].ljust(max_item_string_length.to_i + 1,' ')
+        ary << transposed_items[this_item_x][this_item_y]
+        this_item_y += 1
+      end
+      this_item_x += 1
+      ary.each_with_index do |a, i|
+        if i % (number_of_columns * this_item_x) == 0
+        print "\n"
+        end
       end
     end
-  end
 end
 
-
-if options['l'] == true
+def method_for_l(items)
   items.each do |item|
     file_path = File.absolute_path(item.to_s)
     file_stat = File.stat(file_path)
@@ -85,11 +73,11 @@ if options['l'] == true
     permission_number_ary = file_mode_2octal_permission.to_s.split('')
     file_type = file_stat.ftype
     if file_type == 'directory'
-      file_type = 'd'
+        file_type = 'd'
     elsif file_type == 'file'
-      file_type = '-'
+        file_type = '-'
     elsif file_type == 'link'
-      file_type - 'l'
+        file_type - 'l'
     end
     file_size = file_stat.size
     file_time_mtime = file_stat.mtime
@@ -99,39 +87,42 @@ if options['l'] == true
     file_time_mtime_min = file_time_mtime.min
     file_link = file_stat.nlink 
     permission_number_ary.each_with_index do |n, i|
-      if n == '1' && i == 0
+        if n == '1' && i == 0
         n = 'r'
-      elsif n == '1' && i == 3
+        elsif n == '1' && i == 3
         n = 'r'
-      elsif n == '1' && i == 6
+        elsif n == '1' && i == 6
         n = 'r'
-      elsif n == '1' && i == 1
+        elsif n == '1' && i == 1
         n = 'w'
-      elsif n == '1' && i == 4
+        elsif n == '1' && i == 4
         n = 'w'
-      elsif n == '1' && i == 7
+        elsif n == '1' && i == 7
         n = 'w'
-      elsif n == '1' && i == 2
+        elsif n == '1' && i == 2
         n = 'x'
-      elsif n == '1' && i == 5
+        elsif n == '1' && i == 5
         n = 'x'
-      elsif n == '1' && i == 8
+        elsif n == '1' && i == 8
         n = 'x'
-      else
+        else
         n = '-' 
-      end
-      permission_number_ary[i] = n
+        end
+        permission_number_ary[i] = n
     end
     permission = permission_number_ary.join('')
     owner_uid = file_stat.uid
     file_owner = Etc.getpwuid(owner_uid).name
     group_uid = file_stat.gid
     file_group = Etc.getgrgid(group_uid).name
-
-    
     print file_type + permission + ' ' + file_link.to_s.rjust(2,' ') + ' ' + file_owner + ' ' + file_group  + ' ' + file_size.to_s.rjust(7,' ') + ' ' + file_time_mtime_m.to_s.rjust(2,' ') + ' ' + file_time_mtime_d.to_s + ' ' + file_time_mtime_hour.to_s.rjust(2,'0') + ':' + file_time_mtime_min.to_s.rjust(2,'0') + ' ' + item.to_s
     print "\n"
   end
 end
-print "\n"
 
+if options['l'] == true
+    method_for_l(items)
+else options['l'] != true
+    method_for_general(items,number_of_columns,max_item_string_length)
+end
+print "\n"
