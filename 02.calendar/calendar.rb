c31001b754b3b6dd75199afc14eb67067e06efe3
Miho Uchida <44660466+miho2929@users.noreply.github.com>
2021-07-22 23:28:57 +0900

    余計なコードの削除

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -15,18 +15,6 @@ month = if options["m"]
           Time.now.month
         end
 
-
-if options["y"] && options["m"]
-  year =  options["y"].to_i
-  month = options["m"].to_i
-elsif options["m"]
-  year = Time.now.year
-  month = options["m"].to_i
-else
-  year = Time.now.year
-  month = Time.now.month
-end
-
 this_month_date = Date.new(year, month, -1).day
 week = ["日", "月", "火", "水", "木", "金", "土"]
 header = month.to_s + "月" + " " + year.to_s
