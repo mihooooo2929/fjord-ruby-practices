3fd78db3bbdfca232521047780647934c2f7ce6d
Miho Uchida <44660466+miho2929@users.noreply.github.com>
2021-07-22 16:29:51 +0900

    nextの書き方からbreakに変更

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -22,7 +22,7 @@ frames = shots.each_slice(2).to_a
 point = 0
 
 frames.each_with_index do |frame, i|
-  next unless i < NUMBER_OF_FRAMES
+  break unless i < NUMBER_OF_FRAMES
 
   point += if frame[0] == STRIKE
              if frames[i + 1][0] == STRIKE
