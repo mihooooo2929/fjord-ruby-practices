ff59ec4abb62f2863629be46f853d8d6129451b8
Miho Uchida <44660466+miho2929@users.noreply.github.com>
2021-07-22 15:01:18 +0900

    繰り返し書いていた記述を一度だけ書くようにした

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -17,18 +17,18 @@ frames = shots.each_slice(2).to_a
 
 point = 0
 frames.each_with_index do |frame, i|
-  if frame[0] == 10 # strike
-    if i < 10
-      point += if frames[i + 1][0] == 10
-                 frames[i + 1][0] + frames[i + 2][0] + 10
-               else
-                 frames[i + 1].sum + 10
-               end
-    end
-  elsif frame.sum == 10 # spare
-    point += frames[i + 1][0] + 10 if i < 10
-  elsif i < 10
-    point += frame.sum
-  end
+  next unless i < 10
+
+  point += if frame[0] == 10 # strike
+             if frames[i + 1][0] == 10
+               frames[i + 1][0] + frames[i + 2][0] + 10
+             else
+               frames[i + 1].sum + 10
+             end
+           elsif frame.sum == 10 # spare
+             frames[i + 1][0] + 10
+           else
+             frame.sum
+           end
 end
 puts point
