0055d751142d9dc695ddf5a572e682b55946a0ad
Miho Uchida <44660466+miho2929@users.noreply.github.com>
2021-07-22 16:25:56 +0900

    変数から定数に変更

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+NUMBER_OF_FRAMES = 10
+STRIKE = 10
+SPARE = 10
+
 score = ARGV[0]
 scores = score.split(',')
 shots = []
@@ -16,20 +20,18 @@ end
 frames = shots.each_slice(2).to_a
 
 point = 0
-number_of_frame = 10
-strike = 10
-spare = 10
-frames.each_with_index do |frame, i|
-  next unless i < number_of_frame
 
-  point += if frame[0] == strike
-             if frames[i + 1][0] == 10
-               frames[i + 1][0] + frames[i + 2][0] + 10
+frames.each_with_index do |frame, i|
+  next unless i < NUMBER_OF_FRAMES
+
+  point += if frame[0] == STRIKE
+             if frames[i + 1][0] == STRIKE
+               frames[i + 1][0] + frames[i + 2][0] + STRIKE
              else
-               frames[i + 1].sum + 10
+               frames[i + 1].sum + STRIKE
              end
-           elsif frame.sum == spare
-             frames[i + 1][0] + 10
+           elsif frame.sum == SPARE
+             frames[i + 1][0] + SPARE
            else
              frame.sum
            end
