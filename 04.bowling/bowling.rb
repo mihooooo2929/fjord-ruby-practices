2d060ad8493b34c4631cd2f20af8273825287265
Miho Uchida <44660466+miho2929@users.noreply.github.com>
2021-07-22 15:15:53 +0900

    マジックナンバーがなくなるよう修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -16,16 +16,19 @@ end
 frames = shots.each_slice(2).to_a
 
 point = 0
+number_of_frame = 10
+strike = 10
+spare = 10
 frames.each_with_index do |frame, i|
-  next unless i < 10
+  next unless i < number_of_frame
 
-  point += if frame[0] == 10 # strike
+  point += if frame[0] == strike
              if frames[i + 1][0] == 10
                frames[i + 1][0] + frames[i + 2][0] + 10
              else
                frames[i + 1].sum + 10
              end
-           elsif frame.sum == 10 # spare
+           elsif frame.sum == spare
              frames[i + 1][0] + 10
            else
              frame.sum
