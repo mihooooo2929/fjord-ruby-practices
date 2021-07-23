4ee835497ff589203d0e3c22d86aa0958e56292f
Miho Uchida <44660466+miho2929@users.noreply.github.com>
2021-07-23 15:13:41 +0900

    lオプションが取れるようにした

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -8,14 +8,21 @@ number_of_line = File.read(arg).count("\n")
 words_ary = File.read(arg).split(/\s+/)
 number_of_words = words_ary.count
 number_of_letter = words_ary.join.size
-ary = [arg, number_of_line, number_of_words, number_of_letter]
-arg, number_of_line, number_of_words, number_of_letter = ary.map do |a|
+ary = [number_of_line, number_of_words, number_of_letter, arg]
+
+number_of_line, number_of_words, number_of_letter, arg = ary.map do |a|
   a.to_s.rjust(8, ' ')
 end
 
-def join_all(string_a, string_b, string_c, string_d)
-  string_a + string_b + string_c + string_d
+def print_joined_string_args(strings_ary)
+  print strings_ary.join
 end
 
-print join_all(number_of_line, number_of_words, number_of_letter, arg)
+strings_ary = if options['l']
+                [number_of_line, arg]
+              else
+                [number_of_line, number_of_words, number_of_letter, arg]
+              end
+
+print_joined_string_args(strings_ary)
 print "\n"
